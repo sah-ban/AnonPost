3d6348d8edeea2061cbb76364ed99eda5911dd04
sahban <sahbaan@proton.me>
2025-11-11 19:09:00 +0530

    update fid

--- src/app/api/create-cast/route.ts
+++ src/app/api/create-cast/route.ts
@@ -10,7 +10,7 @@ import {
 import { hexToBytes } from "@noble/hashes/utils";
 import axios from "axios";
 
-const fid = 545039;
+const fid = 1009125;
 const SIGNER = process.env.PRIVATE_KEY || "";
 
 type CastType = "cast" | "reply" | "quote";
