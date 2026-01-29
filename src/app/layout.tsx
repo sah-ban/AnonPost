90fb67071ed71622fa0013cc0a0434fa6f822a9b
sahban <sahbaan@proton.me>
2026-01-29 16:24:06 +0530

    update description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,9 @@ import type { Metadata } from "next";
 import "~/app/globals.css";
 import { Providers } from "../app/providers";
 
-
 export const metadata: Metadata = {
   title: "AnonPost",
-  description: "Post anonymously, for free",
+  description: "Post anonymously on Farcaster",
 };
 
 export default async function RootLayout({
@@ -13,7 +12,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
   return (
     <html lang="en">
       <body>
